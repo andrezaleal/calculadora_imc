c0a1d9e430bdb4dc02b9c3ef230195d7d6e49ab3
Andreza Leal <andrezaleal12@gmail.com>
2023-09-13 22:41:43 -0300

    add testes em dart

--- test/calculadora_imc_test.dart
+++ test/calculadora_imc_test.dart
@@ -1,8 +1,26 @@
-import 'package:calculadora_imc/calculadora_imc.dart';
+import 'package:calculadora_imc/classes/pessoa.dart';
+import 'package:calculadora_imc/exceptions/double_invalido_exception.dart';
+import 'package:calculadora_imc/exceptions/nome_invalido_exception.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(42, 42);
+ test('Teste de cálculo de IMC', () {
+    final pessoa = Pessoa("Denis", 1.75, 50.0);
+
+    final resultado = pessoa.calculadoraImc();
+
+    expect(resultado, "16.33"); 
+  });
+
+test('Teste da exceção NomeInvalidoException', () {
+    expect(() {
+      throw NomeInvalidoException();
+    }, throwsA(TypeMatcher<NomeInvalidoException>()));
+  });
+
+test('Teste da exceção DoubleInvalidoException', () {
+    expect(() {
+      throw DoubleInvalidoException();
+    }, throwsA(TypeMatcher<DoubleInvalidoException>()));
   });
 }
